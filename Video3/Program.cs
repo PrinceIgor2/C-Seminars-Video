c894978f86f9ed68081861607a23addab02302af
Igor <kugimn@list.ru>
2022-09-18 02:27:53 +0300

    Video3 ready

---
+++ Video3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// uint number = 10;
+// short number = 30000;
+// float num = 4.55355355f;
+// string word = "прювет";
+// char symbol = 'ю';
+// bool isHappy = true;
+// Console.WriteLine(word + num + "Пока" + symbol);
+
+int num1 = 0, num2 = 0;
+num1 = Convert.ToInt32(Console.ReadLine());
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Первое число " + num1 + ", второе число " + num2 + ".");
