ef1449d6f783c41597064f8a54b1182a2c78c8e2
Igor <kugimn@list.ru>
2022-09-18 01:44:53 +0300

    Video2 +

---
+++ Video2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hello, World 1\n\n\n");
+Console.WriteLine("Hello, World 2");
+// строчный комментарий
+Console.WriteLine("Hello, World 3");
+// Console.WriteLine("Hello, World 4");
+
+/*
+огромный
+коментарий
+*/
+Console.ReadLine();
