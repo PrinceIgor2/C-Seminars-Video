d4bad43e40b3d505ded92bf2c8fd488026cdda95
Igor <kugimn@list.ru>
2022-09-25 22:09:34 +0300

    Commit 25 09 22

---
+++ GeekC#Lesson2New/Program.cs
@@ -0,0 +1,167 @@
+﻿// // int Max (int arg1, int arg2, int arg3);
+// // {
+// //     int result = arg1;
+// //     if(arg2 > result) result = arg2;
+// //     if(arg3 > result) result = arg3;
+// //     return result;
+// // }
+
+// // int a = 67;
+// // int b = 17;
+// // int c = 8989;
+// // int d = 67;
+// // int e = 47;
+// // int f = 167;
+// // int g = 687;
+// // int h = 34;
+// // int i = 459;
+
+// // int max1 = Max(a,b,c);
+// // Console.WriteLine(max1);
+// // int max2 = Max(d,e,f);
+// // Console.WriteLine(max2);
+// // int max3 = Max(g,h,i);
+// // Console.WriteLine(max3);
+// // int MaxTotal = Max(max1, max2, max3);
+
+// // // if(b > max) max = b;
+// // // if(c > max) max = c;
+
+// // // if(d > max) max = d;
+// // // if(e > max) max = e;
+// // // if(f > max) max = f;
+
+// // // if(g > max) max = g;
+// // // if(h > max) max = h;
+// // // if(i > max) max = i;
+
+// // Console.WriteLine(MaxTotal);
+
+
+
+
+// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
+// //              0  1  2  3  4 5  6  8  9
+// int[] array = {11,22,33,34,35,6,97,18,49};
+
+
+// int max = Max();
+
+
+// // int max1 = Max(a1, b1, c1);
+// // int max2 = Max(a2, b2, c2);
+// // int max3 = Max(a3, b3, c3);
+// // int max = Max(max1, max2, max3);
+
+// // int max = Max(
+// //     Max(a1, b1, c1), 
+// //     Max(a2, b2, c2),
+// //     Max(a3, b3, c3));
+
+// Console.WriteLine(max);
+
+
+// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
+//  //              0    1   2   3   4   5   6   7   8
+// int[] array = { 11, 211, 31, 41, 15, 61, 17, 18, 19 };
+
+// int result = Max(
+//     Max(array[0], array[1], array[2]),
+//     Max(array[3], array[4], array[5]),
+//     Max(array[6], array[7], array[8])
+// );
+// Console.WriteLine(result);
+
+// int[] array = {1,6,8,7,9,4,7,9,4};
+// int n = array.Length;
+// int find = 7;
+// int index = 0;
+// while(index < n){
+// if(array[index] == find){
+//     Console.WriteLine(index);
+//     break;
+// }
+//     index++;
+// }
+ 
+void FillArray(int[] collection){
+
+int length = collection.Length;
+int index = 0;
+while(index < length){
+    
+    collection[index] = new Random().Next(1, 10);
+    index++;
+}
+}
+
+void PrintArray(int[]col){
+    int count = col.Length;
+    int position = 0;
+    while(position < count){
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOF(int[] collection, int find);
+{
+int count = collection.Length;
+int index = 0;
+int position = -1;
+while(index < count){
+if(collection[index] == find)
+{
+ position = index;
+}
+    index++;
+}
+return position;
+}
+
+// int[] array = new int[10];
+
+// FillArray(array);
+// PrintArray(array);
+
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
---
+++ GeekC#Seminar2/Program.cs
@@ -0,0 +1,146 @@
+﻿
+
+//     9 Напишите программу, которая выводит случайное число из отрезка [10, 99]
+//  и показывает наибольшую цифру числа. 
+// 78 -> 8
+//  12-> 2
+//   85 -> 8
+
+// int GetRandomNumberFromRange(int minBorder, int maxBorder)
+// {
+// int result = new int();
+// result = new Random().Next(minBorder, maxBorder+1);
+// return result;
+// }
+
+// int maxDigitFronNumber(int number)
+// {
+// int maxNumber = 0;
+// while(number > 0){
+// int currentDigit = number % 10;
+// if(maxNumber < currentDigit) {
+//     maxNumber = currentDigit;
+//     }
+// number = number / 10;
+// }
+// return maxNumber;
+// }
+
+
+// int getMaxDigitFromMunberOfTwoDigits (int number){
+
+// int MaxDigit = number % 10;
+// int secondDigit = number / 10;
+// if(MaxDigit < secondDigit) {
+//     MaxDigit = secondDigit;
+// }
+// return MaxDigit;
+// }
+// int randomNumber = GetRandomNumberFromRange(10, 99);
+// Console.WriteLine($" Случайное значение {randomNumber}");
+// int maxDigit = getMaxDigitFromMunberOfTwoDigits(randomNumber);
+// Console.WriteLine($" Максимальная цифра случайного значения {maxDigit}");
+
+
+
+//  Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. 456 -> 46 782 -> 72 918 -> 98
+// int number = new Random().Next(100, 1000);
+// Console.WriteLine($"Случайное число {number}");
+// {
+// int MaxDigit = 0;
+
+// while(number > 0){
+// int currentDigit = number % 10;
+// if(MaxDigit < currentDigit) {
+//     MaxDigit = currentDigit;
+//     }
+// number = number / 10;
+// }
+// return MaxDigit;
+// }
+
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10;
+
+
+// Console.WriteLine($"{firstDigit} {thirdDigit}");
+
+
+
+
+
+// 3 Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число кратным первому. 
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления. 
+// 34, 5 -> не кратно, // остаток 4 16, // 4 -> кратно
+
+// int firstnumber = new Random().Next(1,100);
+// int secondnumber = new Random().Next(1,100);
+
+// void ReturnOstatok(int firstnumber, int secondnumber){
+//     if(firstnumber % secondnumber == 0){
+//         Console.WriteLine($"Второе число {secondnumber} кратно первому числу {firstnumber}");
+//             }
+//     else
+//     Console.WriteLine($"Второе число {secondnumber} не кратно первому числу {firstnumber}, остаток {firstnumber % secondnumber}");}
+// Console.WriteLine($"Первое число {firstnumber}"); 
+// Console.WriteLine($"Второе число {secondnumber}"); 
+// ReturnOstatok(firstnumber, secondnumber); 
+
+
+
+// //  Напишите программу, которая выводит случайное трёхзначное число и 
+// удаляет вторую цифру этого числа. 456 -> 46 782 -> 72 918 -> 98
+
+// int number = new Random().Next(100,1000);
+
+// int DeleteSecondDigit(int number){
+
+// int firstnumber = number / 100;
+// int thirdnumber = number % 10;
+// return (firstnumber*10 + thirdnumber);
+// }
+// Console.WriteLine(number);
+// int result = DeleteSecondDigit(number);
+// Console.WriteLine(result);
+
+
+// 4 Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23. 14 -> нет 46 -> нет 161 -> да
+
+// int number = new Random().Next(100,1000);
+
+// string Kratno(int number){
+//     string Kratno = " ";
+//     if(number % 7 == 0 && number % 23 == 0){
+//         Console.WriteLine("Да");
+//     }
+//     else
+//     Console.WriteLine("Нет");
+//     return Kratno;
+// }
+// Console.WriteLine(number);
+// string result = Kratno(number);
+// Console.WriteLine(result);
+
+
+
+
+// 5  Напишите программу, которая принимает на вход два числа и проверяет,
+//  является ли одно число квадратом другого. 
+//  5, 25 -> да 
+//  - 4, 16 -> да
+//   25, 5 -> да
+//   8,9 -> нет
+
+
+Console.WriteLine("Введите первое число ");
+int usernumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int usernumberB = Convert.ToInt32(Console.ReadLine());
+if(usernumberB == Math.Pow(usernumberA,2)){
+    Console.WriteLine("Да");
+}
+else{
+Console.WriteLine("Нет");
+}
